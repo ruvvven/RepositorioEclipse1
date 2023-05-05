7658ce2e66ef1fbcc735046eb802b9be147a23f3
Diurno <Diurno@pc0118>
2023-05-05 13:05:41 +0200

    Primera confirmación

---
+++ Proyecto1Git/src/git/Principal.java
@@ -0,0 +1,11 @@
+package git;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Control de versiones con git");
+
+	}
+
+}
---
+++ Proyecto1Git/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Diurno
+ *
+ */
+module Proyecto1Git {
+}
